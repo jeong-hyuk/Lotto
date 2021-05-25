78f1df2e853345bb77925fe85916ff17fb3c47b3
jeong-hyuk <jack1150@naver.com>
2021-05-25 09:50:25 +0900

    Intent

--- app/src/main/java/com/google/lotto/MainActivity.kt
+++ app/src/main/java/com/google/lotto/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.google.lotto
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.cardview.widget.CardView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val cardRandom = findViewById<CardView>(R.id.cardRandom)
+        cardRandom.setOnClickListener {
+            val intent = Intent(this, ResultActivity::class.java)
+            startActivity(intent)
+        }
+        val cardConstell = findViewById<CardView>(R.id.cardConstell)
+        cardConstell.setOnClickListener {
+            startActivity(Intent(this, ConstellationActivity::class.java))
+        }
+        val cardName = findViewById<CardView>(R.id.cardName)
+        cardName.setOnClickListener {
+            startActivity(Intent(this, NameActivity::class.java))
+        }
     }
 }

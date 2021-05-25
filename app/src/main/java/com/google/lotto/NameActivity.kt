c8ec26449be9ff375a8a8787a7a0481834448a25
jeong-hyuk <jack1150@naver.com>
2021-05-25 09:31:58 +0900

    Result activity

--- app/src/main/java/com/google/lotto/NameActivity.kt
+++ app/src/main/java/com/google/lotto/NameActivity.kt
@@ -1,11 +1,22 @@
 package com.google.lotto
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class NameActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_name)
+        val button = findViewById<Button>(R.id.button)
+        val button2 = findViewById<Button>(R.id.button2)
+
+        button.setOnClickListener {
+            startActivity(Intent(this@NameActivity, ConstellationActivity::class.java))
+        }
+        button2.setOnClickListener {
+            startActivity(Intent(this@NameActivity, ConstellationActivity::class.java))
+        }
     }
 }
